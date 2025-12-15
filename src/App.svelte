15dffcd81243399bfed07214c06c32bea6a0c613
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2025-12-14 22:18:38 -0600

    flesh out about markdown, add social logos, transition some css to app component instead of global

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
   import { marked } from 'marked';
-  import blurbMd from './assets/blurb.md?raw';
   import { onMount } from 'svelte';
+  import blurbMd from './assets/blurb.md?raw';
+  import aboutMd from './assets/about.md?raw';
+  import linkedinLogo from './assets/linkedin-logo.png';
+  import githubLogo from './assets/github-logo.png';
+  import linktreeLogo from './assets/linktree-logo.png';
+  import chevron from './assets/chevron.png';
+
+  // State ================
+  let mainImageBottomEdge = $state<undefined | number>(undefined);
+
+  // ======================
   
   // For client-side, just construct URLs directly
   const urlEndpoint = "https://ik.imagekit.io/3sz0exlmi";
@@ -11,6 +21,7 @@
   
   // Convert markdown to HTML
   const blurbHtml = marked(blurbMd);
+  const aboutHtml = marked(aboutMd);
 
   // typewriter animation ================
   const textValue = "Melvin Gruschow"
@@ -42,6 +53,13 @@
 
     }, intervalValue)
 
+    setTimeout(() => {
+      document.getElementById('links')!.classList.add('fade-in');
+    }, delay)
+
+    document.getElementById('main-image')!.addEventListener('load', () => {
+      mainImageBottomEdge = document.getElementById('main-image')!.getBoundingClientRect().bottom;
+    });
   })
 
 
@@ -54,16 +72,61 @@
     &nbsp;
     <span id="blinking-cursor">_</span>
   </div>
+
+  <div class="links-container" id="links">
+    <a class="social-link" href="https://www.linkedin.com/in/melvin-gruschow/" target="_blank"><img src={linkedinLogo} alt="LinkedIn" aria-label="Link to Melvin Gruschow's LinkedIn profile" title="LinkedIn" /></a>
+    <a class="social-link" href="https://www.github.com/mfgruschow" target="_blank"><img src={githubLogo} alt="GitHub" aria-label="Link to Melvin Gruschow's GitHub profile" title="GitHub" /></a>
+    <a class="social-link" href="https://linktr.ee/fealios" target="_blank"><img src={linktreeLogo} alt="LinkTree" aria-label="Link to Melvin Gruschow's LinkTree profile" title="LinkTree" /></a>
+  </div>
 </header>
 
 <main>
-  <div class="blurb" id="first-blurb">
-    {@html blurbHtml}
+  <div id="image-blurb-wrapper">
+    <div class="blurb" id="first-blurb">
+      {@html blurbHtml}
+    </div>
+    <img id="main-image" class="main-image" src={motorcycleImagePath} alt="Main" aria-label="Main" />
+    <img id="chevron" src={chevron} alt="Chevron" aria-label="Chevron"  />
   </div>
-  <img class="main-image" src={motorcycleImagePath} alt="Main" aria-label="Main" />
 
+  <div id="about-container">
+    <div>
+      {@html aboutHtml}
+    </div>
+  </div>
+  
 </main>
 
 <style>
+  .links-container {
+    visibility: hidden;
+    margin-top: 10px;
+
+    .social-link {
+      color: initial;
+      text-decoration: none;
+
+      img {
+        width: 2rem;
+        height: auto;
+      }
+    }
+  }
+
+  #image-blurb-wrapper {
+    position: relative;
+    #chevron {
+      position: absolute;
+      left: 50%;
+      bottom: 3rem;
+      width: 4rem;
+    }
+  }
+  
+  #about-container {
+    color: white;
+    z-index: var(--main-z-value);
+  }
+
 
 </style>
---
+++ src/assets/about.md
@@ -0,0 +1,18 @@
+### About Me
+
+As a self taught software engineer, I have always made strides to be a strong self starter and my own favorite teacher. Reddit posts, Github discussions, Mozilla news blogs, Tiktok videos, Discord channels, you can find me spread out across the internet discussing tools, tips and tricks with seasoned vets and newbies.
+<br />
+<br />
+Having worked for some large name companies, including Microsoft and Nintendo, I have a host of experience dealing with clients who demand perfection and have harsh deadlines. My skillsets on the frontend span almost every major JavaScript based framework, almost exclusively integrated with Typescript, staying at the cutting edge of the ecosystem.  
+<br />
+<br />
+Even my hobbies detail my obsession with solving puzzles and creating unique ideas. I spend a great amount of time bouldering, which for those who are unaware is rock climbing without ropes. In competition environments, you are timed on your ability to analyze a problem, and execute a solution with little to no room for errors. Mistakes lead to injuries and losses, so it is my job to plan accordingly.  
+<br />
+<br />
+I also spent much of my leisure time reading and writing, having published a short story with another on the way, while working on two novels, one a youth fantasy story and another a murder mystery. When writing something as large scale as a book the details matter, and complexities that can span across years of your life as a developer (oops, I meant writer) need to be carefully thought out.
+<br />
+<br />
+I have taken these skillsets as well as my thirst for more knowledge and started my own consulting company, Drizzly Consulting LLC. With this small business, I challenge myself with fast turnaround times, having completed a [complex portfolio site](https://corinne-originals.vercel.app/) with a large set of high definition images for a client within two weeks. This project, coded in Next.JS with Typescript, Tailwind, and deployed on Vercel, involved using technologies I was unfamiliar with, as well as being completely designed by myself, yet I accepted the challenge and have received high praise from my client.  
+<br />
+<br />
+It is these skills and my mindset that I believe, makes me an invaluable asset to a team, and I am proud of the work I have done, and look forward to all that I am still to do.
